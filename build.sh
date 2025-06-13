b6ac6ad146c88f94c0767488c4f0d63a2f745c43
萝莉璃 <49371494+luolili233@users.noreply.github.com>
2025-06-13 22:57:07 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 appName="alist"
 builtAt="$(date +'%F %T %z')"
-gitAuthor="Xhofe <i@newalist.top>"
+gitAuthor="luolili233 <i@newalist.top>"
 gitCommit=$(git log --pretty=format:"%h" -1)
 
 if [ "$1" = "dev" ]; then
